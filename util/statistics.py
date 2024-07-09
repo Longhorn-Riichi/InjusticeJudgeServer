ee371c4692c218b7c54c362abca42bb8a54a058f
Daniel Wang <danielwang9@protonmail.com>
2024-07-08 21:54:59 -0500

    Fix total game counting

--- util/statistics.py
+++ util/statistics.py
@@ -14,9 +14,9 @@ class Statistics:
             ssl=True)
 
     def process_game_injustices(self, all_results: List[Dict[int, List[CheckResult]]]):
+        self.client.hincrby("statistics", "total_games", 1)
+        self.client.hincrby("statistics", "total_kyokus", len(all_results))
         for result in all_results:
-            self.client.hincrby("statistics", "total_games", 1)
-            self.client.hincrby("statistics", "total_kyokus", len(all_results))
             for seat, injustices in result.items():
                 for injustice in injustices:
                     self.client.hincrby("statistics", "injustice_" + injustice.identifier, 1)
