b13f8913dd80dc348c102f158cc434132159c02b
Daniel Wang <danielwang9@protonmail.com>
2024-07-08 21:30:07 -0500

    Log some statistics

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import dotenv
 import os
 import re
 from util.gateway import Gateway
+from util.statistics import Statistics
 from quart import Quart, jsonify, request
 from InjusticeJudge.injustice_judge.fetch.majsoul import MahjongSoulAPI, parse_majsoul, MahjongSoulAPI
 from InjusticeJudge.injustice_judge.fetch.tenhou import parse_tenhou
 from InjusticeJudge.injustice_judge.fetch.riichicity import RiichiCityAPI, parse_riichicity
-from InjusticeJudge.injustice_judge.injustices import evaluate_game
+from InjusticeJudge.injustice_judge.injustices import format_result, get_results
 
 app = Quart(__name__)
 gateway = None
+statistics = Statistics()
 majsoul_regex = r"https://mahjongsoul.game.yo-star.com/\?paipu=([a-z0-9]{6}-[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})(_a\d+)?(_[0-3])?"
 tenhou_regex = r"https://tenhou.net/0/\?log=(\d{10}gm-[0-9a-f]{4}-\d{4,}-[0-9a-f]{8})(&tw=\d+)?"
 riichicity_regex = r"[a-z0-9]{20}(@.+)?"
 
 @app.route('/injustice', methods=['POST'])
 async def run_injustice():
+    global gateway
+    global statistics
     data = await request.get_json()
     link = data['link']
     print(link)
@@ -32,19 +36,27 @@ async def run_injustice():
             break
     else:
         raise Exception("Invalid input")
+
     player = parsed_player_seat if parsed_player_seat is not None else player
     if player is None:
         try:
-            return [result for kyoku in kyokus for result in evaluate_game(kyoku, {0,1,2,3}, parsed_metadata.name)]
+            all_results = [get_results(kyoku, {0,1,2,3}) for kyoku in kyokus]
         except:
-            return [result for kyoku in kyokus for result in evaluate_game(kyoku, {0,1,2}, parsed_metadata.name)]
+            all_results = [get_results(kyoku, {0,1,2}) for kyoku in kyokus]
     else:
-        return [result for kyoku in kyokus for result in evaluate_game(kyoku, {player}, parsed_metadata.name)]
+        all_results = [get_results(kyoku, {player}) for kyoku in kyokus]
+    statistics.process_game_injustices(all_results)
+    return [format_result(seat, result_list, parsed_metadata.name, len(results) == 1)
+            for results in all_results
+            for seat, result_list in results.items()
+            if len(result_list) > 0]
 
 async def run():
     dotenv.load_dotenv("config.env")
     async with MahjongSoulAPI(mjs_username=os.getenv("ms_username"), mjs_password=os.getenv("ms_password"), mjs_uid=os.getenv("ms_uid"), mjs_token=os.getenv("ms_token")) as ms_api:
+        print("Logged into Mahjong Soul!")
         async with RiichiCityAPI("aga.mahjong-jp.net", os.getenv("rc_email"), os.getenv("rc_password")) as rc_api:
+            print("Logged into Riichi City!")
             global gateway
             gateway = Gateway(ms_api=ms_api, rc_api=rc_api)
 
