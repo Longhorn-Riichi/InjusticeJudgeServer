717df3f79212c6ea6f1f57bb1f6e05738c10aa1e
Daniel Wang <danielwang9@protonmail.com>
2024-07-11 22:24:12 -0500

    Support /skill invocations

--- main.py
+++ main.py
@@ -9,30 +9,39 @@ from InjusticeJudge.injustice_judge.fetch.majsoul import MahjongSoulAPI, parse_m
 from InjusticeJudge.injustice_judge.fetch.tenhou import parse_tenhou
 from InjusticeJudge.injustice_judge.fetch.riichicity import RiichiCityAPI, parse_riichicity
 from InjusticeJudge.injustice_judge.injustices import format_result, get_results
+from typing import *
 
 app = Quart(__name__)
-gateway = None
+gateway: Optional[Gateway] = None
 statistics = Statistics()
 majsoul_regex = r"https://mahjongsoul.game.yo-star.com/\?paipu=([a-z0-9]{6}-[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})(_a\d+)?(_[0-3])?"
 tenhou_regex = r"https://tenhou.net/0/\?log=(\d{10}gm-[0-9a-f]{4}-\d{4,}-[0-9a-f]{8})(&tw=\d+)?"
 riichicity_regex = r"[a-z0-9]{20}(@.+)?"
 
-@app.route('/injustice', methods=['POST'])
-async def run_injustice():
+@app.route('/injustice', methods=['POST'])  # type: ignore[type-var]
+async def run_injustice() -> None:
+    await call({"injustice"})
+
+@app.route('/skill', methods=['POST'])  # type: ignore[type-var]
+async def run_skill() -> None:
+    await call({"skill"})
+
+async def call(look_for: Set[str] = {"injustice"}):
     global gateway
     global statistics
+    assert gateway is not None
     data = await request.get_json()
     link = data['link']
     print(link)
-    for regex, fetch, parse in [[majsoul_regex, gateway.fetch_majsoul, parse_majsoul],
-                                [tenhou_regex, gateway.fetch_tenhou, parse_tenhou],
-                                [riichicity_regex, gateway.fetch_riichicity, parse_riichicity]]:
+    for regex, fetch, parse in [(majsoul_regex, gateway.fetch_majsoul, parse_majsoul),
+                                (tenhou_regex, gateway.fetch_tenhou, parse_tenhou),
+                                (riichicity_regex, gateway.fetch_riichicity, parse_riichicity)]:
         if re.match(regex, link) is not None:
             try:
                 log, metadata, player = await fetch(link)
             except: # try it again
                 log, metadata, player = await fetch(link)
-            kyokus, parsed_metadata, parsed_player_seat = parse(log, metadata, None)
+            kyokus, parsed_metadata, parsed_player_seat = parse(log, metadata, None)  # type: ignore[operator]
             break
     else:
         raise Exception("Invalid input")
@@ -40,11 +49,11 @@ async def run_injustice():
     player = parsed_player_seat if parsed_player_seat is not None else player
     if player is None:
         try:
-            all_results = [get_results(kyoku, {0,1,2,3}) for kyoku in kyokus]
+            all_results = [get_results(kyoku, {0,1,2,3}, look_for) for kyoku in kyokus]
         except:
-            all_results = [get_results(kyoku, {0,1,2}) for kyoku in kyokus]
+            all_results = [get_results(kyoku, {0,1,2}, look_for) for kyoku in kyokus]
     else:
-        all_results = [get_results(kyoku, {player}) for kyoku in kyokus]
+        all_results = [get_results(kyoku, {player}, look_for) for kyoku in kyokus]
     statistics.process_game_injustices(all_results)
     return [format_result(seat, result_list, parsed_metadata.name, len(results) == 1)
             for results in all_results
